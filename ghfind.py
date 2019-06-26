a9a370024520fffdd5d9f5228fe023e06700d16d
Kai Longmore <14970686+handle-git@users.noreply.github.com>
2019-06-26 15:59:45 +1000

    Add file via upload

---
+++ ghfind.py
@@ -0,0 +1,7 @@
+from requests_html import HTMLSession, sys
+lines, i = HTMLSession().get("https://api.github.com/users/" + sys.argv[1] + "/events/public").text.split("\n"), -1
+for x in range(4):
+	for y in range(max(x, i), len(lines)):
+		i += 1
+		if ["payload", "commits", "author", sys.argv[2]][x] in lines[i]: break		
+print("\n" + sys.argv[2] + ": " + lines[i][lines[i].find(sys.argv[2]) + len(sys.argv[2]) + 4:-(len(sys.argv[2]) - 3)] + "\n")
